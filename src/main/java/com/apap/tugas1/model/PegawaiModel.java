c98622e08acf44d786acf0b513bd7bc13adba6b7
roktav <oktavio.rei@gmail.com>
2018-10-19 22:05:13 +0700

    fitur 2 clear

--- src/main/java/com/apap/tugas1/model/PegawaiModel.java
+++ src/main/java/com/apap/tugas1/model/PegawaiModel.java
@@ -8,7 +8,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.util.Date;
+
+import java.sql.Date;
 import java.util.List;
 
 @Entity
